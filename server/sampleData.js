a9ae728f9c76dc22a59b9c31394be4e98c034cdc
Qodestackr <winchygichu@gmail.com>
2022-07-06 16:44:51 +0300

    first commit

---
+++ server/sampleData.js
@@ -0,0 +1,74 @@
+// Projects
+const projects = [
+  {
+    name: 'Build Mobile App',
+    description:
+      'Use React Native with Expo to roll out a travel companion application',
+    status: 'Rejected',
+  },
+  {
+    name: 'Community reading',
+    description:
+      'We will engage in a community reading practice for 3-4hrs',
+    status: 'Complete',
+  },
+  {
+    name: 'SEO Project',
+    description:
+      'Add SEO to my JAMstack site',
+    status: 'In Progress',
+  },
+  {
+    name: 'Design Prototype',
+    description:
+      'Design a prototype and roll out an MVP in two weeks',
+    status: 'Complete',
+  },
+  {
+    name: 'Simulation Tests',
+    description:
+      'Build a simulation on airports programmes',
+    status: 'In Progress',
+  },
+]
+
+// Clients
+const users = [
+  {
+    id: '1',
+    name: 'Tony Stark',
+    email: 'ironman@gmail.com',
+    phone: '360-900-9000',
+    role: 'admin'
+  },
+  {
+    id: '2',
+    name: 'John Doe',
+    email: 'blackwidow@gmail.com',
+    phone: '280-568-3320',
+    role: 'viewer'
+  },
+  {
+    id: '3',
+    name: 'Jane Doe',
+    email: 'thor@gmail.com',
+    phone: '324-331-4333',
+    role: 'viewer'
+  },
+  {
+    id: '4',
+    name: 'Steve Rogers',
+    email: 'steve@gmail.com',
+    phone: '344-562-6787',
+    role: 'viewer'
+  },
+  {
+    id: '5',
+    name: 'Bruce Banner',
+    email: 'bruce@gmail.com',
+    phone: '321-468-8887',
+    role: 'admin'
+  },
+]
+
+module.exports = { projects, clients }
